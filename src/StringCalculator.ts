24fd50595638bc1a4a588d99b4b5d7bad95c16c5
ravel01106 <amandaravelocabrera@gmail.com>
2024-03-21 15:47:06 +0000

    refactor(StringCalculator): we add the alternative separator when we create the regular expression

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,7 +1,7 @@
 export class StringCalculator {
-  add(numbers: String): number {
+  add(numbers: string): number {
     let result: number = 0
-    let separator: string = ",|\n"
+    let separator: string = ","
 
     if (numbers == "") return 0
     else if (numbers.length == 1) return Number(numbers)
@@ -11,7 +11,7 @@ export class StringCalculator {
       numbers = numbers.substring(numbers.indexOf("\n"))
     }
 
-    let separatorRegx: RegExp = new RegExp(separator)
+    let separatorRegx: RegExp = new RegExp(separator + "|\n")
 
     this.checkNumberSeparators(numbers)
 
@@ -27,12 +27,12 @@ export class StringCalculator {
   private createCustomSeparator(numbers: String): string {
     let separator = numbers.substring(2, numbers.indexOf("\n"))
 
-    separator = this.isVerticalBar(separator) ? "\\||\n" : separator + "|\n"
+    separator = this.isVerticalBar(separator) ? "\\|" : separator
 
     return separator
   }
 
-  private checkNumberSeparators(numbers: String) {
+  private checkNumberSeparators(numbers: string) {
     if (numbers.endsWith(",") || numbers.endsWith("\n")) {
       throw new Error(`Number expected but EOF found.`)
     } else if (numbers.includes(",\n") || numbers.includes("\n,")) {
