877f377d5975c4120801200967b83b9a98a1f748
ravel01106 <ravello2002@gmail.com>
2024-04-08 11:55:59 +0100

    refactor(StringCalculator.spec.ts): changed test names

--- src/net/aprendices/test/StringCalculator.spec.ts
+++ src/net/aprendices/test/StringCalculator.spec.ts
@@ -2,92 +2,86 @@ import { describe, it, expect } from "vitest"
 import { StringCalculator } from "../main/StringCalculator.js"
 
 describe("String calculator should", () => {
-  // Devuelve 0 cuando no hay ninguna expresión
-  // No deberia sumar ningun número cuando no hay expresión
-  it(" return 0 when an empty string is passed.", () => {
+  it(" not add any number when there is no expression.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("")
 
     expect(result).toBe(0)
   })
-  // Mantiene el mismo número cuando solo recibe un número
-  it(" return the number when given only a number.", () => {
+
+  it(" keep the same number when receiving a single number.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("2")
 
     expect(result).toBe(2)
   })
-  // Deberia sumar números separados por comas
-  it(" return the total sum when given numbers separated by commas.", () => {
+
+  it(" add numbers separated by commas.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("1,2,3")
 
     expect(result).toBe(6)
   })
 
-  // Deberia sumar números decimales separados por comas
-  it(" return the total sum when given decimals separated by commas.", () => {
+  it(" add decimals separated by commas.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("1.1,2.2,3.3")
 
     expect(result).toBe(6.6)
   })
-  // Deberia sumar números decimales separados por comas o salto de linea
-  it(" return the total sum when given numbers separated by commas or newlines.", () => {
+
+  it(" add numbers separated by commas or newlines.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("1\n2,3")
 
     expect(result).toBe(6)
   })
 
-  // lanza error cuando ...
-  it(" return error when there are two separators together.", () => {
+  it(" throw error when there are two separators together.", () => {
     const stringCalculator = new StringCalculator()
     expect(() => stringCalculator.add("175.2,\n35")).toThrowError(
       /^Number expected but '\\n' found at the position 6.$/,
     )
   })
 
-  // lanza ...
-  it(" return error when the last character is a separator.", () => {
+  it(" throw error when the last character is a separator.", () => {
     const stringCalculator = new StringCalculator()
     expect(() => stringCalculator.add("1,3,")).toThrowError(/^Number expected but EOF found.$/)
   })
 
-  // change
-  it(" return the total sum when given numbers separated by custom separator.", () => {
+  it(" add numbers separated by custom separator.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("//;\n1;2")
 
     expect(result).toBe(3)
   })
 
-  it(" return the total sum when given numbers separated by another custom separator.", () => {
+  it(" add numbers separated by another custom separator.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("//sep\n2sep3")
 
     expect(result).toBe(5)
   })
 
-  it(" return the total when given numbers separated by vertical bar separator.", () => {
+  it(" add numbers separated by vertical bar separator.", () => {
     const stringCalculator = new StringCalculator()
     const result = stringCalculator.add("//|\n1|2|3")
 
     expect(result).toBe(6)
   })
 
-  it(" return error when there is a separator other than the custom separator.", () => {
+  it(" throw error when there is a separator other than the custom separator.", () => {
     const stringCalculator = new StringCalculator()
     expect(() => stringCalculator.add("//|\n1|2,3")).toThrowError(/^'\\|' expected but ',' found at position 3.$/)
   })
 
-  it(" return error when there are negative numbers.", () => {
+  it(" throw error when there are negative numbers.", () => {
     const stringCalculator = new StringCalculator()
     expect(() => stringCalculator.add("-1,2")).toThrowError(/^Negative not allowed : -1.$/)
   })
 
   // throw error with all negative numbers
-  it(" return error with all negative numbers when it has.", () => {
+  it(" throw error with all negative numbers when it has.", () => {
     const stringCalculator = new StringCalculator()
     expect(() => stringCalculator.add("2,-4,-5")).toThrowError(/^Negative not allowed : -4, -5.$/)
   })
